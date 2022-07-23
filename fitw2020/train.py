47770785d43d17182733b126a385fa1fa749bd33
Warley Vital <vitalwarley@gmail.com>
2022-07-23 11:18:18 -0300

    adds validation to shadrikov script

--- fitw2020/train.py
+++ fitw2020/train.py
@@ -17,6 +17,7 @@ from tqdm import tqdm
 # guild
 from fitw2020 import mytypes as t
 from fitw2020.dataset import FamiliesDataset
+from val_shadrikov import log_results
 
 gpu = 0
 jitter_param = 0.15
@@ -38,26 +39,10 @@ end_lr = 1e-10
 normalize = False
 
 
-class ReJPGTransform(gluon.nn.Block):
-    def __init__(self, prob: float = 0.5, min_quality: int = 50):
-        super(ReJPGTransform, self).__init__()
-        self.prob = prob
-        self.min_quality = min_quality
-
-    def forward(self, x):
-        if np.random.rand() < self.prob:
-            quality = np.random.randint(self.min_quality, 99)
-            img = x.asnumpy()
-            _, encoded = cv2.imencode(".jpg", img, [cv2.IMWRITE_JPEG_QUALITY, quality])
-            decoded = mx.img.imdecode(encoded).astype(np.float32)
-            x = decoded
-        return x
-
-
-def train(normalize: bool = False):
+def train():
     # init dataset
     train_dataset = FamiliesDataset(Path("fiw/train-faces-det"))
-    val_dataset = FamiliesDataset(Path("fiw/val-faces-det"))
+    # val_dataset = FamiliesDataset(Path("fiw/val-faces-det"))
     # init paths
     weight_path = str("models/arcface_r100_v1-0000.params")
     # weight_path = str(f'snapshots_ft/{net_name}-0040.params')
@@ -86,7 +71,7 @@ def train(normalize: bool = False):
         ]
     )
 
-    transform_img_val = transforms.Compose([transforms.ToTensor()])
+    # transform_img_val = transforms.Compose([transforms.ToTensor()])
     # sets dataloaders
     train_data = mx.gluon.data.DataLoader(
         train_dataset.transform_first(transform_img_train),
@@ -96,13 +81,13 @@ def train(normalize: bool = False):
         pin_memory=True,
     )
     # not used
-    val_data = mx.gluon.data.DataLoader(
-        train_dataset.transform_first(transform_img_val),
-        shuffle=False,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=True,
-    )
+    # val_data = mx.gluon.data.DataLoader(
+    #     train_dataset.transform_first(transform_img_val),
+    #     shuffle=False,
+    #     batch_size=batch_size,
+    #     num_workers=num_workers,
+    #     pin_memory=True,
+    # )
     # gpu device
     ctx = ctx_list[0]
     # loads model architecture
@@ -111,7 +96,7 @@ def train(normalize: bool = False):
     if normalize:
         norm_sym = mx.sym.sqrt(mx.sym.sum(sym**2, axis=1, keepdims=True) + 1e-6)
         sym = mx.sym.broadcast_div(sym, norm_sym, name="fc_normed") * 32
-    # adds clasification layer
+    # adds classification layer
     sym = mx.sym.FullyConnected(
         sym, num_hidden=num_families, name="fc_classification", lr_mult=1
     )
@@ -163,10 +148,25 @@ def train(normalize: bool = False):
     output_dir.mkdir(exist_ok=True, parents=True)
     writer = SummaryWriter(str(output_dir))
 
-    import torchmetrics as tm
+    # debug with my datamodule
+    from tasks import init_fiw
 
-    acc_tm = tm.Accuracy()
-    acc_tm_preds = tm.Accuracy()
+    datamodule = init_fiw(
+        data_dir="fiw",
+        batch_size=batch_size,
+        mining_strategy="baseline",
+        num_workers=8,
+    )
+    datamodule.setup("fit")
+    train_dataloader = datamodule.train_dataloader()
+    n_train_batches = len(train_dataloader)
+    val_dataloader = datamodule.val_dataloader()
+    n_val_batches = len(val_dataloader)
+
+    def cosine(emb1, emb2):
+        _sum = np.sum(emb1 * emb2, axis=-1)
+        _norms = np.linalg.norm(emb1, axis=-1) * np.linalg.norm(emb2, axis=-1)
+        return _sum / _norms
 
     for epoch in range(num_epoch):
         if epoch == lr_steps[lr_counter]:
@@ -177,7 +177,9 @@ def train(normalize: bool = False):
         losses = [0] * len(all_losses)
         metric = mx.metric.Accuracy()
         print(" > training", epoch)
-        for i, batch in tqdm(enumerate(train_data), total=len(train_data)):
+        for i, batch in tqdm(enumerate(train_dataloader), total=n_train_batches):
+            batch = [batch[0].numpy(), batch[1].numpy().astype(np.int32)]
+            # for i, batch in tqdm(enumerate(train_data), total=len(train_data)):
             if warmup_iter < warmup:
                 cur_lr = (warmup_iter + 1) * (lr - start_lr) / warmup + start_lr
                 trainer.set_learning_rate(cur_lr)
@@ -208,17 +210,10 @@ def train(normalize: bool = False):
                     [cur_loss(o, l) for (o, l) in zip(outputs, gts)]
                     for _, cur_loss in all_losses
                 ]
-                metric.update(gts, outputs)
-                gts_ = torch.tensor([gt.asnumpy() for gt in gts], device="cpu").squeeze(
-                    0
-                )
-                outs_ = torch.tensor(
-                    [out.asnumpy() for out in outputs], device="cpu"
-                ).squeeze(0)
-                acc_tm.update(outs_, gts_)
-                acc_tm_preds.update(outs_.argmax(dim=1), gts_)
                 combined_losses = [cur[0] for cur in zip(*cur_losses)]
-                if np.any([np.any(np.isnan(l.asnumpy())) for l in cur_losses[0]]):
+                if np.any(
+                    [np.any(np.isnan(loss_.asnumpy())) for loss_ in cur_losses[0]]
+                ):
                     print("OOps2!")
                     raise RuntimeError
             for combined_loss in combined_losses:
@@ -226,41 +221,61 @@ def train(normalize: bool = False):
 
             trainer.step(batch_size, ignore_stale_grad=True)
             for idx, cur_loss in enumerate(cur_losses):
-                losses[idx] += sum([l.mean().asscalar() for l in cur_loss]) / len(
-                    cur_loss
-                )
+                losses[idx] += sum(
+                    [loss_.mean().asscalar() for loss_ in cur_loss]
+                ) / len(cur_loss)
 
-        if (epoch + 1) % 10 == 0:
-            net.save_parameters(
-                str(snapshots_path / f"{net_name}-{(epoch + 1):04d}.params")
-            )
+        # if (epoch + 1) % 10 == 0:
+        #     net.save_parameters(
+        #         str(snapshots_path / f"{net_name}-{(epoch + 1):04d}.params")
+        #     )
 
-        losses = [l / num_batch for l in losses]
+        losses = [loss_ / num_batch for loss_ in losses]
         losses_str = [
             f"{l_name}: {losses[idx]:.3f}" for idx, (l_name, _) in enumerate(all_losses)
         ]
         losses_str = "; ".join(losses_str)
         m_name, m_val = metric.get()
         losses_str += f"| {m_name}: {m_val}"
-        writer.add_scalar(f"train/loss", losses[idx], epoch)
-        writer.add_scalar(f"train/accuracy", m_val, epoch)
-        writer.add_scalar(f"train/accuracy_tm", acc_tm.compute().item(), epoch)
-        writer.add_scalar(
-            f"train/accuracy_tm_preds", acc_tm_preds.compute().item(), epoch
-        )
-        writer.add_scalar(f"lr", trainer.learning_rate, epoch)
+        writer.add_scalar("train/loss", losses[idx], epoch)
+        writer.add_scalar("train/accuracy", m_val, epoch)
+        writer.add_scalar("lr", trainer.learning_rate, epoch)
         print(f"[Epoch {epoch:03d}] {losses_str} | time: {time.time() - tic:.1f}")
 
-        acc_tm.reset()
-        acc_tm_preds.reset()
+        print(" > validating", epoch)
+        distances = []
+        similarities = []
+        labels = []
+        for i, batch in tqdm(enumerate(val_dataloader), total=n_val_batches):
+            # img1, img2, label
+            batch = [batch[0].numpy(), batch[1].numpy(), batch[2].numpy()]
+            img1 = mx.gluon.utils.split_and_load(
+                batch[0] * 255, ctx_list=ctx_list, even_split=False
+            )[0]
+            img2 = mx.gluon.utils.split_and_load(
+                batch[1] * 255, ctx_list=ctx_list, even_split=False
+            )[0]
+            embeddings1 = net(img1).asnumpy()
+            embeddings2 = net(img2).asnumpy()
+            sim = cosine(embeddings1, embeddings2)
+            normed_emb1 = embeddings1 / np.linalg.norm(
+                embeddings1, axis=-1, keepdims=True
+            )
+            normed_emb2 = embeddings2 / np.linalg.norm(
+                embeddings2, axis=-1, keepdims=True
+            )
+            diff = normed_emb1 - normed_emb2
+            dist = np.linalg.norm(diff, axis=-1)
+            distances.append(dist)
+            similarities.append(sim)
+            labels.append(batch[2])
 
-        # TODO: compute verification accuracy and auc (as in val_shadrikov.py)
-        # print(' > validating', epoch)
-        # TODO: would need to use PairDataset for this; also, add val_pairs.csv there.
-        # for i, batch in tqdm(enumerate(val_data), total=len(val_data)):
-        #     data = mx.gluon.utils.split_and_load(batch[0] * 255, ctx_list=ctx_list, even_split=False)
-        #     gts = mx.gluon.utils.split_and_load(batch[1], ctx_list=ctx_list, even_split=False)
-        #     outputs = [net(X) for X in data]
+        distances = np.concatenate(distances)
+        similarities = np.concatenate(similarities)
+        labels = np.concatenate(labels)
+        best_threshold, best_accuracy, auc_score = log_results(
+            writer, "val", distances, similarities, labels
+        )
 
     net.export(str(snapshots_path / "net"))
 
@@ -268,4 +283,4 @@ def train(normalize: bool = False):
 if __name__ == "__main__":
     np.random.seed(100)
     mx.random.seed(100)
-    train(normalize=normalize)
+    train()
